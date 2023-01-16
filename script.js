04a50084748d99ef14cdf8b773cdfdccd7f42f61
Tharindu Madhushan <59754026+tharinm@users.noreply.github.com>
2023-01-16 13:36:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var i = 0;
 var images = [];
 var time = 2000;
 
-images[0] = "/images/img1.jpg";
-images[1] = "/images/img2.jpg";
-images[2] = "/images/img3.jpg";
-images[3] = "/images/img4.jpg";
-images[4] = "/images/img5.jpg";
+images[0] = "images/img1.jpg";
+images[1] = "images/img2.jpg";
+images[2] = "images/img3.jpg";
+images[3] = "images/img4.jpg";
+images[4] = "images/img5.jpg";
 
 function changeImage() {
   document.slide.src = images[i];
